033ac7a3912d498fd2020964b91f5cfda080eaa8
Lyst67 <p_listopad@ukr.net>
2024-03-07 20:46:11 +0200

    fix-redux

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,27 @@
-import { useDispatch } from 'react-redux'
-import css from './Filter.module.css'
-import { filterContacts } from 'redux/contacts/filterSlice'
-import {ImSearch} from "react-icons/im";
-
-
+import { useDispatch } from "react-redux";
+import css from "./Filter.module.css";
+import { filterContacts } from "../../app/contacts/filterSlice";
+import { ImSearch } from "react-icons/im";
 
 export const Filter = () => {
-const dispatch = useDispatch()
-const handleFilter = (event) => {
-    const value = event.target.value
-   dispatch(filterContacts(value))
-    }
-   
-    return (
-        <div className={css.filter_form}>
-            <label className={css.filter_label}><ImSearch/>
-                <input className={css.filter_input} type="text" name="filter" placeholder='Start typing a name...'
-                     onChange={handleFilter}></input>
-            </label>
-        </div>
-    )
-}  
+  const dispatch = useDispatch();
+  const handleFilter = (event) => {
+    const value = event.target.value;
+    dispatch(filterContacts(value));
+  };
+
+  return (
+    <div className={css.filter_form}>
+      <label className={css.filter_label}>
+        <ImSearch />
+        <input
+          className={css.filter_input}
+          type="text"
+          name="filter"
+          placeholder="Start typing a name..."
+          onChange={handleFilter}
+        ></input>
+      </label>
+    </div>
+  );
+};
